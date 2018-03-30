292e8f02503e4033c74952848596930df5fc420f
kcarter92 <33101173+kcarter92@users.noreply.github.com>
2018-03-29 21:42:30 -0500

    React #2 Homework

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return(
+    <div className="jumbotron container">
+      <h1>New York Times Articles</h1>
+      <hr/>
+      <h4>What do you want to know!</h4>
+    </div>
+  )
+};
+
+export default Header;
